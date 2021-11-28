ea480f520787d6250289ddcfb76b5dcf6740ce2a
anhvinhphan659 <anhvinhphan659@gmail.com>
2021-11-28 14:10:59 +0700

    update main.js

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -222,17 +222,3 @@
     });
 
 })(jQuery);
-
-
-//other js for pagination
-$('#paginating').pagination({
-    dataSource: [1, 2, 3, 4, 5, 6, 7, 8],
-    pageSize: 5,
-    showPrevious: false,
-    showNext: false,
-    callback: function(data, pagination) {
-        // template method of yourself
-        var html = template(data);
-        dataContainer.html(html);
-    }
-})
